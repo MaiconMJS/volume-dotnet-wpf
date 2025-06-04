35631a20fb803e534574f93c663e36a2a2cf0d42
MaiconMJS <newoverride@gmail.com>
2025-06-04 11:16:19 -0300

    Função arrastar circle finalizada

--- HomeView.xaml.cs
+++ HomeView.xaml.cs
@@ -1,7 +1,8 @@
 ﻿using MahApps.Metro.Controls;
-
 using System.ComponentModel;
 using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
 using Application = System.Windows.Application;
 
 namespace Volume
@@ -11,17 +12,51 @@ namespace Volume
         private NotifyIcon? _trayIcon;
         private ContextMenuStrip? _trayMenu;
 
+        private bool _isDragging = false;
+        private System.Windows.Point _dragStartPoint;
+
         public HomeView()
         {
             InitializeComponent();
             SetupTrayIcon();
         }
 
+        private void ControlCircle_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            _isDragging = true;
+            _dragStartPoint = e.GetPosition(this);
+            ControlCircle.CaptureMouse();
+        }
+
+        private void ControlCircle_MouseLeftButtonUp(object sender, MouseButtonEventArgs e) 
+        {
+            _isDragging = false;
+            ControlCircle.ReleaseMouseCapture();
+        }
+
+        private void ControlCircle_MouseMove(object sender, System.Windows.Input.MouseEventArgs e)
+        {
+            if (_isDragging)
+            {
+                System.Windows.Point currentPosition = e.GetPosition(this);
+                double deltaY = currentPosition.Y - _dragStartPoint.Y;
+                
+                double currentTop = Canvas.GetTop(ControlCircle);
+                double newTop = currentTop + deltaY;
+
+                newTop = Math.Max(20, Math.Min(260, newTop));
+
+                Canvas.SetTop(ControlCircle, newTop);
+
+                _dragStartPoint = currentPosition;
+            }
+        }
+
         private void SetupTrayIcon()
         {
             _trayMenu = new ContextMenuStrip();
             _trayMenu.Items.Add("Abrir", null, (_, _) => ShowWindow());
-            _trayMenu.Items.Add("Sair", null, (_, _) => 
+            _trayMenu.Items.Add("Sair", null, (_, _) =>
             {
                 _trayIcon?.Dispose();
                 _trayIcon = null;
